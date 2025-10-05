d35b783bb25aa41684fb1c16667a6d30157a83b3
101476000 <101476000@georgebrown.ca>
2025-10-05 11:36:07 -0400

    including application source

---
+++ week05_lab_execrcise05/week05_lab_execrcise05/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <main style={{ fontFamily: "system-ui, Arial", padding: 24 }}>
+      <h1>Week05 Lab – React SPA</h1>
+      <p>Carlos Figuera - ID 101476000</p>
+    </main>
+  );
+}
